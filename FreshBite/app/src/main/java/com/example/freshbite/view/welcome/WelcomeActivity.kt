b087b8842fb7a0981b01b36b43d79c0a020a7ca4
Suryo Aji Widagdo <suryoaji.wdg@gmail.com>
2024-06-03 18:02:01 +0700

    update welcome page design

--- FreshBite/app/src/main/java/com/example/freshbite/view/welcome/WelcomeActivity.kt
+++ FreshBite/app/src/main/java/com/example/freshbite/view/welcome/WelcomeActivity.kt
@@ -54,7 +54,7 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun playAnimation() {
-        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+        ObjectAnimator.ofFloat(binding.AppTitle, View.TRANSLATION_X, -30f, 30f).apply {
             duration = 6000
             repeatCount = ObjectAnimator.INFINITE
             repeatMode = ObjectAnimator.REVERSE
