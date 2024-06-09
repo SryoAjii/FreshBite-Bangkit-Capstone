570cbfe5a903890f6bd2eb874aac271fd9a207fe
Darma Cahya <darmady6@gmail.com>
2024-06-10 02:40:11 +0700

    fix auth api

--- CC/app/middleware/verifySignUp.js
+++ CC/app/middleware/verifySignUp.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const ROLES = db.Roles;
+const ROLES = db.ROLES;
 const User = db.user;
 
 checkDuplicateUsernameOrEmail = async (req, res, next) => {
--- CC/app/models/index.js
+++ CC/app/models/index.js
@@ -25,12 +25,8 @@ db.sequelize = sequelize;
 db.user = require("./userModel.js")(sequelize, Sequelize);
 db.role = require("./roleModel.js")(sequelize, Sequelize);
 
-db.role.belongsToMany(db.user, {
-  through: "user_roles"
-});
-db.user.belongsToMany(db.role, {
-  through: "user_roles"
-});
+db.role.belongsToMany(db.user, { through: "user_roles", foreignKey: "roleId", otherKey: "userId" });
+db.user.belongsToMany(db.role, { through: "user_roles", foreignKey: "userId", otherKey: "roleId" });
 
 db.ROLES = ["user", "admin"];
 
