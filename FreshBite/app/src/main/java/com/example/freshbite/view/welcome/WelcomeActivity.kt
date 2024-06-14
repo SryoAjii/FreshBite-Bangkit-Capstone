e65d4dc34fcf3d91d6678b1b0e3a93ce6131167b
Suryo Aji Widagdo <suryoaji.wdg@gmail.com>
2024-06-14 11:07:42 +0700

    redesign welcome page

--- FreshBite/app/src/main/java/com/example/freshbite/view/welcome/WelcomeActivity.kt
+++ FreshBite/app/src/main/java/com/example/freshbite/view/welcome/WelcomeActivity.kt
@@ -43,25 +43,20 @@ class WelcomeActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
-
-        binding.signupButton.setOnClickListener {
-            startActivity(Intent(this, SignupActivity::class.java))
-        }
     }
 
     private fun playAnimation() {
-        ObjectAnimator.ofFloat(binding.AppTitle, View.TRANSLATION_X, -30f, 30f).apply {
+        ObjectAnimator.ofFloat(binding.logo, View.TRANSLATION_X, -30f, 30f).apply {
             duration = 6000
             repeatCount = ObjectAnimator.INFINITE
             repeatMode = ObjectAnimator.REVERSE
         }.start()
 
         val login = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(100)
-        val signup = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(100)
         val desc = ObjectAnimator.ofFloat(binding.descTextView, View.ALPHA, 1f).setDuration(100)
 
         val together = AnimatorSet().apply {
-            playTogether(login, signup)
+            playTogether(login)
         }
 
         AnimatorSet().apply {
